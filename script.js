b0efe47adbd9c24141b0327ee7061aff032c1d93
AlexanderCherentcov <asmosurgxx@gmail.com>
2024-05-28 20:24:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -35,4 +35,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
         projectGrid.appendChild(projectItem);
     });
+
+    // Animation on scroll
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
+        projectGrid.appendChild(projectItem);
+    });
 });
